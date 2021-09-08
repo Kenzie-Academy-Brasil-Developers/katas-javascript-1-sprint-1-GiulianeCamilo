815e0704db96107a7432503055bf39ec6f4d0fd9
Giuliane Camilo <giuliane.cjm@gmail.com>
2021-09-08 12:40:35 -0300

    pronto

--- katas1.js
+++ katas1.js
@@ -1,7 +1,7 @@
 function oneThroughTwenty() {
    let resultado = [];
    for (let contagem = 1; contagem <= 20; contagem++){
-    resultado += contagem
+    resultado += contagem + " "
 
    }
    return resultado
@@ -12,7 +12,7 @@ function evensToTwenty() {
     let resultado = [];
   for (let pares = 1; pares <= 20; pares++){
     if (pares % 2 ===0)
-    resultado += pares
+    resultado += pares + " "
   }
   return resultado
 }
@@ -22,7 +22,7 @@ function oddsToTwenty() {
     let resultado = [];
   for (let impares = 0; impares < 20; impares++) {
     if (impares % 2 != 0) {
-      resultado += impares 
+      resultado += impares + " "
 }
   }
   return resultado
@@ -34,7 +34,7 @@ function multiplesOfFive() {
   let resultado = [];
   for(let multiplos = 5 ; multiplos <= 100 ; multiplos++){
 		if(multiplos % 5 ==0){
-			resultado += multiplos
+			resultado += multiplos + " "
 		}
 	}  
   return resultado
@@ -45,7 +45,7 @@ function multiplesOfFive() {
  function squareNumbers() {
   let resultado = [];
   for (let quadrados = 1; quadrados < 10; quadrados++) {
-    resultado += quadrados
+    resultado += quadrados + " "
   }
   return resultado
 
@@ -56,7 +56,7 @@ console.log(squareNumbers())
 function countingBackwards() {
   let resultado = [];
   for (let i = 20; i >= 1; i--){
-    resultado += i
+    resultado += i + " "
 }
 return resultado
 
@@ -68,7 +68,7 @@ return resultado
     let resultado = [];
     for (let pares = 20; pares >= 1; pares--){
       if (pares % 2 ===0)
-      resultado += pares
+      resultado += pares + " "
     }
     return resultado
 
@@ -79,7 +79,7 @@ function oddNumbersBackwards() {
    let resultado = [];  
   for (let impares = 20; impares > 1; impares--) {
     if (impares % 2 != 0) {
-      resultado += impares 
+      resultado += impares + " "
 }
   }
   return resultado
@@ -91,7 +91,7 @@ function oddNumbersBackwards() {
     let resultado = [];
     for(let multiplos = 100 ; multiplos >= 5 ; multiplos--){
       if(multiplos % 5 ==0){
-        resultado += multiplos
+        resultado += multiplos + " "
       }
     }  
     return resultado
@@ -102,7 +102,7 @@ console.log(multiplesOfFiveBackwards())
 function squareNumbersBackwards() {
     let resultado = [];
       for (let quadrados = 10; quadrados > 1; quadrados--) {
-      resultado += quadrados
+      resultado += quadrados + " "
     }
     return resultado
 
